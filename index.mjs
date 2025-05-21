8a8e81bb3ff945b0067e91cbf06bfa39be8ce344
Suryansh-Dey <deysuryansh@gmail.com>
2025-05-21 19:43:31 +0530

    Adding response streaming

--- index.mjs
+++ index.mjs
@@ -1,14 +1,17 @@
 import fetchMarkdown from "./function.js"
 import { intoTree, namifyTree } from './utils.js'
+import util from 'util';
+import stream from 'stream';
+const { Readable } = stream;
+const pipeline = util.promisify(stream.pipeline);
 
-export async function handler(event) {
+export const handler = awslambda.streamifyResponse(async (event, responseStream, _context) => {
+    event = JSON.parse(event.body) //For function URL
     let pages = await fetchMarkdown(new URL(event.url), { images: event.images }, event.limit);
     if (event.tree) {
         pages = intoTree(pages)
-        namifyTree(pages)
+        await namifyTree(null, null, pages)
     }
-    return {
-        statusCode: 200,
-        body: pages
-    }
-}
+    const requestStream = Readable.from(Buffer.from(JSON.stringify(pages)));
+    await pipeline(requestStream, responseStream);
+});
